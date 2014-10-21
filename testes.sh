1206a6345884d6020978e5db45491908ac06b41c
pedrofilho88 <pedrofilho88@github.com>
2014-10-21 10:37:06 -0200

    novas alterações no script

--- testes.sh
+++ testes.sh
@@ -5,15 +5,15 @@ while (true) do
 	
 	#status="abcABC123ABCabc" # zera variavel status antes de começar
 	sleep 1	
-	#status=$(curl http://localhost:3000/votacao/estagio)
+	status=$(curl http://localhost:3000/votacao/estagio)
     echo
 	echo
-	read status
+	echo
 	echo "valor lido foi: " $status
-	echo "tamanho foi:"  ${#status}
-	echo "o último caracter foi:  " ${status:0:${#status}-2}
-	echo $status
-	echo ${status: -1}
+	echo 
+	echo 
+	echo 
+	echo 
 	echo	
 
 
