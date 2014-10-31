fb709cc29e392fd38e512cd6b798aef3d751e406
pedrofilho88 <pedrofilho88@github.com>
2014-10-31 00:40:36 -0200

    feitas novas impemanetaçoes no radio.h e apagado mudaEstado()

--- proj_votacao_interface/radio.h
+++ proj_votacao_interface/radio.h
@@ -4,16 +4,14 @@
 /*------------------------------------------------------------------------------*/
 #define CE_PIN   9 //pino 9 do microcontrolador
 #define CSN_PIN 10 //pino 10 do microcontrolador
-// NOTA: define o endereço mac do dispositivo a ser transmitido
-const uint64_t recep = 0xE8E8F0F0E2LL; // define o mac do receptor
-const uint64_t trans = 0xE8E8F0F0E1LL;// define o mac do transmissor
+#define nro_dispositivos 3 // o numero de receptores
+// NOTA: recep define os endereços MAC doS dispositivos a serem recebidos
+const uint64_t recep[nro_dispositivos] = { 0xE8E8F0F0E2LL, 0xE8E8F0F0E3LL, 0xE8E8F0F0E4LL };
+const uint64_t trans[nro_dispositivos] = { 0xE8E8F0F0E2LL, 0xE8E8F0F0E3LL, 0xE8E8F0F0E4LL };
 RF24 radio(CE_PIN, CSN_PIN); // cria uma instancia de "RF24" chamada "radio"
-void mudaEstado(bool flag);
-
 //**********************************************************************************
-
 void inicializaRadio(){
-      printf_begin();
+      printf_begin(); // inicializa a função printf
       radio.begin();//inicializa o rádio
       radio.setDataRate(RF24_250KBPS);//define a velocidade de transmissao
       radio.setPALevel(RF24_PA_MAX);//ajusta a pontencia de transmissao
@@ -21,10 +19,38 @@ void inicializaRadio(){
       radio.enableDynamicPayloads();// (tenho que estudar isso ainda!)
       radio.setRetries(15,15);// ajusta o valor máximo de tentativas
       radio.setCRCLength(RF24_CRC_16);//define tamanho do dado transmitido
-      mudaEstado(true); //inicializa o radio a prinicipio como receptor
-}//fim_inicializaRadio()*************************************************************
+} //fim_inicializaRadio()*************************************************************
 
-void mudaEstado(bool flag){
+void modoRecepcao(){ // Entra em listening e espera algum dado via radio
+      for(int y=0; y<nro_dispositivos; y++){
+        radio.openReadingPipe(y,recep[y]); //abre a comunicação como receptor
+      }
+      radio.startListening();//começa a receber os dados de todos os dispositivos
+}
+
+void enviaTodos(char *mensagem){ //Envia mensagem para todos endereços
+      radio.stopListening();
+      for(int x=0; x<nro_dispositivos; x++){
+          radio.openWritingPipe(trans[x]);//transmite para endereço especificado
+          delay(5); // aguarda 5 ms
+          radio.write(mensagem, strlen(mensagem)); //transmite mensagem
+      }
+      for(int y=0; y<nro_dispositivos; y++){
+          radio.openReadingPipe(y, recep[y]);//transmite para endereço especificado
+      }
+      radio.startListening();// entra em listening
+}
+
+void enviaEspecifico(uint8_t disp_num, char *mensagem){
+      radio.stopListening();
+      radio.openWritingPipe(trans[disp_num]);
+      delay(5);
+      radio.write(mensagem, strlen(mensagem));
+      modoRecepcao(); //coloca o módulo novamente em modo de recepção
+}
+
+
+/*void mudaEstado(bool flag){
     if(flag){
         radio.openWritingPipe(trans);
         radio.openReadingPipe(1,recep); //abre a comunicação como receptor
@@ -36,3 +62,4 @@ void mudaEstado(bool flag){
         radio.openWritingPipe(trans); //abre a comunicacao como transmissor
     }
 }// fim_mudaEstado()*****************************************************************
+*/
