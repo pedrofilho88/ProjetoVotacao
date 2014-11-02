bc576f20261e91edfa520e42359e0d7f4cfdc8e1
pedrofilho88 <pedrofilho88@github.com>
2014-11-02 14:14:19 -0200

    feita implementação complata do interface.sh, o código está funcional testado e aprovado

--- interface.sh
+++ interface.sh
@@ -1,60 +1,67 @@
 #!/bin/bash
-
 stty 57600 -ixon icanon </dev/ttyUSB0 #abre serial e ajusta baud rate
 sleep 1 #aguarda 1 segundo
 stty -a -F /dev/ttyUSB0 #desabilita reinicializacao
 sleep 1 #aguarda 1 segundo
 exec 3<> /dev/ttyUSB0 #abre serial novamente
 sleep 1 #aguarda 1 segundo
-
-while (true) do
-	
-	status=" " # zera variavel status antes de começar
-	sleep 1	    # espera um segundo a cada iteração
-	status=$(curl http://localhost:3000/votacao/estagio)
-	echo
-	echo
-	echo $status
-	echo
-	echo
-        
-	if [ $status = "presenca" ]; then
-		echo "enviando presenca pela seiral"
-		echo $status > /dev/ttyUSB0
-		echo "agora aguardando resposta do módulo..."
-		read codigo < /dev/ttyUSB0
-		echo "leu: " $codigo		
-		curl -d "codigo="${codigo}"" http://localhost:3000/votacao/presenca
-		echo "enviou o codigo pra o site..."
-		sleep 0.2 # aguarda 200 ms para estabilidade		
-		echo confirmacao > /dev/ttyUSB0  #ENVIA CONFIRMACAO P/ MODULO
-		codigo=" "  # zera variavel codigo para proxima leitura
-		sleep 0.5  # aguarda 500 ms		
-	elif [ $status = "votar" ]; then
-		echo "enviando votar pela serial"
-		echo $status > /dev/ttyUSB0 # envia o status de votar para serial
-		sleep 0.5
-		echo "agora aguardando resposta do módulo..."
-		read voto < /dev/ttyUSB0 # limpa o buffer da serial USB0
-		voto=" " # limpa a variavel voto
-		read  voto < /dev/ttyUSB0 # Lê a senha e o voto musturados
-		echo "lido voto:"$voto
-		echo "numero de caracteres:"${#voto}
-		sleep 0.5 # aguarda 500 ms	
-		codigo=${voto:0:${#voto}-4}	 # voto menos os dois ultimos digitos
-		echo "codigo depois do recorte: " $codigo
-		sleep 5
-		curl -d "codigo="${codigo}"&voto="${voto: (-3)}"" http://localhost:3000/votacao
-		echo "enviou o voto: " ${voto: (-3)}   
-		sleep 0.2
-		echo confirmacao > /dev/ttyUSB0 # ENVIA CONFIRMACAO P/MODULO
-		codigo=" "  # zera variavel codigo para proxima leitura
-		voto=""     # zera variavel  voto  para proxima leitura
-		sleep 0.5    # aguarda 500 ms
+status=" " # zera variavel status antes de começar
+#***********************************************************************************************
+principal(){
+while (true) do	
+	lido_site=" "
+	sleep 0.5  # espera um segundo a cada iteração
+	lido_site=$(curl http://localhost:3000/votacao/estagio) #atribui a lido_site retorno do site	
+	if [[  $status == $lido_site ]]; then
+		decide
 	else
-		echo "sem acao"		
-	fi
-
+		echo $lido_site > /dev/ttyUSB0 #envia comando para serial
+		echo "entrou no primeiro if"
+		status=$lido_site #atribui o valor lido pelo para status
+		echo "status: "$status 
+		echo "lido_site: "$lido_site
+	    decide
+	fi	    
 done
+}
 
-#curl -d "codigo="${codigo}"" http://localhost:3000/votacao/presenca
+decide() {
+	case "$status" in
+		"presenca")	funcaoPresenca;;	#chama função presença		
+			
+		"votar") funcaoVotar;;		
+	esac
+	principal	
+}
+
+funcaoVotar() {	
+	codigo=" "
+	resposta=" "
+	read -t 0.4 voto < /dev/ttyUSB0 # tenta ler alguma coisa por 400 ms	
+	if [[ $voto ]]; then		
+		codigo=${voto:0:${#voto}-2}	 # voto menos os dois ultimos digitos			
+		resposta=$(curl -d "codigo=$codigo&voto=${voto: (-1)}" http://localhost:3000/votacao)		
+		sleep 0.3
+		echo "resposta: " $resposta
+		if [[ $resposta == "ok" ]]; then
+			echo "confirmacao" > /dev/ttyUSB0 # envia resposta para serial
+		fi
+	fi
+	principal	
+}
+${voto_lido:0:${#voto_lido}-4}
+funcaoPresenca() {
+	
+	resposta=" " #zera variavel resposta denovo
+	read -t 0.4 codigo < /dev/ttyUSB0 # tenta ler alguma coisa por 400 ms 
+	if [[ $codigo ]]; then	
+		resposta=$(curl -d "codigo="${codigo}"" http://localhost:3000/votacao/presenca)
+		sleep 0.2
+		if [[ $resposta == "ok" ]]; then
+			echo "resposta ok"
+			echo "confirmacao" > /dev/ttyUSB0 #envia resposta de volta para serial
+		fi		
+	fi
+	principal	
+}
+principal
--- teste_interface.sh
+++ teste_interface.sh
@@ -1,65 +1,60 @@
 #!/bin/bash
+
 stty 57600 -ixon icanon </dev/ttyUSB0 #abre serial e ajusta baud rate
 sleep 1 #aguarda 1 segundo
 stty -a -F /dev/ttyUSB0 #desabilita reinicializacao
 sleep 1 #aguarda 1 segundo
 exec 3<> /dev/ttyUSB0 #abre serial novamente
 sleep 1 #aguarda 1 segundo
-status=" " # zera variavel status antes de começar
-#***********************************************************************************************
-principal(){
-while (true) do	
-	lido_site=" "
-	sleep 0.5  # espera um segundo a cada iteração
-	lido_site=$(curl http://localhost:3000/votacao/estagio) #atribui a lido_site retorno do site	
-	if [[ $status == " " || $status != lido_site ]]; then
-		echo $lido_site > /dev/ttyUSB0 #envia comando para serial
-		status=$lido_site #atribui o valor lido pelo para status
-	    decide
-	else
-		decide 
-	fi	    
-done
-}
 
-
-decide() {
-	case "$status" in
-		"presenca")	funcaoPresenca;;	#chama função presença		
-			
-		"votar") funcaoVotar;;		
-	esac
-	principal	
-}
-
-funcaoVotar() {
-	voto=0
-	codigo=" "
-	resposta=" "
-	read -t 0.4 voto < /dev/ttyUSB0 # tenta ler alguma coisa por 400 ms
-	echo $voto
-	if [[ $voto != " " ]]; then
+while (true) do
+	
+	status=" " # zera variavel status antes de começar
+	sleep 1	    # espera um segundo a cada iteração
+	status=$(curl http://localhost:3000/votacao/estagio)
+	echo
+	echo
+	echo $status
+	echo
+	echo
+        
+	if [ $status = "presenca" ]; then
+		echo "enviando presenca pela seiral"
+		echo $status > /dev/ttyUSB0
+		echo "agora aguardando resposta do módulo..."
+		read codigo < /dev/ttyUSB0
+		echo "leu: " $codigo		
+		curl -d "codigo="${codigo}"" http://localhost:3000/votacao/presenca
+		echo "enviou o codigo pra o site..."
+		sleep 0.2 # aguarda 200 ms para estabilidade		
+		echo confirmacao > /dev/ttyUSB0  #ENVIA CONFIRMACAO P/ MODULO
+		codigo=" "  # zera variavel codigo para proxima leitura
+		sleep 0.5  # aguarda 500 ms		
+	elif [ $status = "votar" ]; then
+		echo "enviando votar pela serial"
+		echo $status > /dev/ttyUSB0 # envia o status de votar para serial
+		sleep 0.5
+		echo "agora aguardando resposta do módulo..."
+		read voto < /dev/ttyUSB0 # limpa o buffer da serial USB0
+		voto=" " # limpa a variavel voto
+		read  voto < /dev/ttyUSB0 # Lê a senha e o voto musturados
+		echo "lido voto:"$voto
+		echo "numero de caracteres:"${#voto}
+		sleep 0.5 # aguarda 500 ms	
 		codigo=${voto:0:${#voto}-4}	 # voto menos os dois ultimos digitos
-		echo "entrou"
-		resposta=$(curl -d "codigo="${codigo}"&voto="${voto: (-3)}"" http://localhost:3000/votacao)
-		if [[ $resposta == "ok" ]]; then
-			echo $resposta > /dev/ttyUSB0 # envia resposta para serial
-		fi
+		echo "codigo depois do recorte: " $codigo
+		sleep 5
+		curl -d "codigo="${codigo}"&voto="${voto: (-3)}"" http://localhost:3000/votacao
+		echo "enviou o voto: " ${voto: (-3)}   
+		sleep 0.2
+		echo confirmacao > /dev/ttyUSB0 # ENVIA CONFIRMACAO P/MODULO
+		codigo=" "  # zera variavel codigo para proxima leitura
+		voto=""     # zera variavel  voto  para proxima leitura
+		sleep 0.5    # aguarda 500 ms
+	else
+		echo "sem acao"		
 	fi
-	principal	
-}
 
+done
 
-funcaoPresenca() {
-	codigo=" "
-	resposta=" " #zera variavel resposta denovo
-	read -t 0.4 codigo < /dev/ttyUSB0 # tenta ler alguma coisa por 400 ms 
-	if [[ $codigo != " " ]]; then	
-		resposta=$(curl -d "codigo="${codigo}"" http://localhost:3000/votacao/presenca)
-		if [[ $resposta == "ok" ]]; then
-			echo $resposta > /dev/ttyUSB0 #envia resposta de volta para serial
-		fi		
-	fi
-	principal	
-}
-principal
+#curl -d "codigo="${codigo}"" http://localhost:3000/votacao/presenca
