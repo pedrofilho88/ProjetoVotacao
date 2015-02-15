89f09e128ee27ea4fb1d29a33b1a5c34e082974d
root <root@pedro-HP>
2015-02-15 18:17:17 -0200

    resolvido bug que ficava encerrando sozinho estourando memória

--- interface.sh
+++ interface.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+#************** inibe  reinicialização da serial ***********************************************
 stty 115200 -ixon icanon </dev/ttyUSB0 #abre serial e ajusta baud rate
 sleep 1 #aguarda 1 segundo
 stty -a -F /dev/ttyUSB0 #desabilita reinicializacao
 sleep 1 #aguarda 1 segundo
 exec 3<> /dev/ttyUSB0 #abre serial novamente
-sleep 1 #aguarda 1 segundo
 status=" " # zera variavel status antes de começar
 resposta=" "
+sleep 8 # Aguarda 5 seg até que o node seja inicializado
+
+cd /home/pedro/principal && ./sobeNode.sh &   #sobe banco de dados
+sleep 8 # Aguarda 8 segundos até o banco subir
+./chamaChrome.sh & #Inicializa o google chrome
+
+
 #********************************  FUNÇÃO PRINCIPAL  **********************************************
 principal(){
 	lido_site=" "	
@@ -15,7 +22,6 @@ principal(){
 	if [[  $status == $lido_site ]]; then
 		decide
 	else
-		#echo $lido_site
 		echo $lido_site > /dev/ttyUSB0 #envia comando para serial
 		status=$lido_site #atribui o valor lido pelo para status
 		decide
@@ -24,64 +30,50 @@ principal(){
 #*******************************  FUNÇÃO DECIDE **************************************************
 decide() {
 	case "$status" in
-		"presenca")	funcaoPresenca;;	#chama função presença		
-			
-		"votar") funcaoVotar;;		
-	esac
-	principal	
+		"presenca")	funcaoPresenca;;	#chama função presença			
+		"votar") funcaoVotar;;		    #chama função votar
+	esac	
 }
 #********************************  FUNÇÃO VOTAR   **********************************************
 funcaoVotar() {	
 	codigo=" "
 	resposta=" "
 	voto=0
-	read -t 0.2 voto < /dev/ttyUSB0 # tenta ler alguma coisa por 400 ms
-	#echo "voto lido"
-	#echo $voto
-	#echo "tamanho"
-	echo ${#voto}
+	read -t 0.1 voto < /dev/ttyUSB0 # tenta ler alguma coisa por 200 ms		
 	if [[ $voto ]]; then
-	    #echo "entrou"
+		#echo "voto inteiro"$voto	    
 	    #echo "voto"${voto: (-1)}		
 		codigo=${voto:0:${#voto}-2}	 # voto menos os dois ultimos digitos
 		voto="${voto: (-1)}"
-		#echo "voto cortado"$voto		
 		#echo $codigo"codigo"
 		resposta=$(curl -d "codigo=$codigo&voto="${voto}"" http://localhost:3000/votacao)		
 		sleep 0.3
-		#echo "resposta foi: " $resposta
 		if [[ $resposta == "ok" ]]; then
-
 			echo "confirmacao" > /dev/ttyUSB0 # envia resposta para serial
 		fi
 		if [[ $resposta == "error" ]]; then
 			echo "error" > /dev/ttyUSB0 # envia resposta para serial
 		fi
-	fi
-	principal	
+	fi		
 }
 #*********************************  FUNÇAO PRESENÇA  *******************************************
 funcaoPresenca() {	
 	resposta=" " #zera variavel resposta denovo
 	codigo=0
 	read -t 0.2 codigo < /dev/ttyUSB0 # tenta ler alguma coisa por 400 ms
-	#echo
-	#echo "codigo lido|"$codigo
-	#echo		
 	if [[ $codigo ]]; then			
-		resposta=$(curl -d "codigo="${codigo}"" http://localhost:3000/votacao/presenca)
+		resposta=$(curl --keepalive-time 3 -d "codigo="${codigo}"" http://localhost:3000/votacao/presenca)
 		sleep 0.2
-		#echo "resposta foi: " $resposta
 		if [[ $resposta == "ok" ]]; then
-			#echo "resposta ok"
 			echo "confirmacao" > /dev/ttyUSB0 #envia resposta de volta para serial
 		fi
 		if [[ $resposta == "error" ]]; then
 			echo "error" > /dev/ttyUSB0 # envia resposta para serial
 		fi				
-	fi
-
-	principal	
+	fi		
 }
-#***********************************  FIM  *********************************************************
-principal
+#*********************************** Chama Função Principal Infinitamente***********************
+
+while (true) do
+	principal
+done
